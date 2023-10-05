ca161cb84476a768d19005893437fd4a2ccf8054
barehands-io <>
2023-10-05 23:00:56 +0100

    add filter here

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { useStore } from "~/stores";
+import {useStore} from "~/stores";
+import axios from "axios";
+import {routes_data, type_rating} from "~/stores/data";
 
 definePageMeta({
   layout: "main",
@@ -8,11 +10,54 @@ definePageMeta({
 
 const store = useStore();
 
-const { data } = await useFetch("http://localhost:3800/all");
+
+const form = ref<any>({
+  organization_name: "",
+  town_city: "",
+  type_rating: "",
+  route: "",
+  search: "",
+});
+
+
+const data = ref<any>();
+
+const isLoading = ref(false);
+
+const error = ref("");
+
+
+function getData() {
+  isLoading.value = true;
+  error.value = "";
+  axios.get("http://localhost:3800/all")
+      .then((res) => {
+
+        console.log(res);
+        data.value = res.data;
+        isLoading.value = false;
+      })
+      .catch((err) => {
+        error.value = err;
+        isLoading.value = false;
+      });
+}
+
+onMounted(() => {
+  getData();
+});
+
+
 </script>
 
 <template>
-  <div class="container mx-auto">
+  <div v-if="isLoading">
+    loading
+  </div>
+
+  <div
+      v-else-if="data?.data?.data"
+      class="container mx-auto">
     <div class="px-4 sm:px-6 lg:px-8">
       {{ data.data.meta }}
 
@@ -37,10 +82,25 @@ const { data } = await useFetch("http://localhost:3800/all");
         </div>
       </div>
       <div>
-        <div class="grid grid-cols-3 gap-8">
+        <div class="grid grid-cols-2 gap-8">
+
+          <div class="flex flex-col">
+            <label>Route:</label>
+
+            <select class="border-2 rounded-md py-2">
+              <option v-for="(item, index) in routes_data" :key="index" :value="item.key"> {{ item.route }}</option>
+            </select>
+          </div>
+          <div class="flex flex-col">
+            <label>Type rating:</label>
+
+            <select class="border-2 rounded-md py-2">
+              <option v-for="(item, index) in type_rating" :key="index" :value="item.key"> {{ item.type_rating }}</option>
+            </select>
+          </div>
           <div class="flex flex-col">
             <label> type_rating</label>
-            <input type="search" class="bg-gray-200 p-2 w-full" />
+            <input type="search" class="bg-gray-200 p-2 w-full"/>
             <p>Search by Company Name town_city type_rating <p class="bg-gray-700">Route</p></p>
           </div>
         </div>
@@ -50,104 +110,104 @@ const { data } = await useFetch("http://localhost:3800/all");
           <div class="inline-block min-w-full py-2 align-middle">
             <table class="min-w-full border-separate border-spacing-0">
               <thead>
-                <tr>
-                  <th
+              <tr>
+                <th
                     scope="col"
                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
-                  >
-                    Company Name
-                  </th>
-                  <th
+                >
+                  Company Name
+                </th>
+                <th
                     scope="col"
                     class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
-                  >
-                    town_city
-                  </th>
-                  <th
+                >
+                  town_city
+                </th>
+                <th
                     scope="col"
                     class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
-                  >
-                    type_rating
-                  </th>
-                  <th
+                >
+                  type_rating
+                </th>
+                <th
                     scope="col"
                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                  >
-                    Role
-                  </th>
-                  <th
+                >
+                  Role
+                </th>
+                <th
                     scope="col"
                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
-                  >
-                    <span class="sr-only">Edit</span>
-                  </th>
-                </tr>
+                >
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
               </thead>
               <tbody>
-                <tr
+              <tr
                   v-for="(person, personIdx) in data.data.data"
                   :key="person.email"
-                >
-                  <td
+              >
+                <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8',
                     ]"
-                  >
-                    {{ person.organization_name }}
-                  </td>
-                  <td
+                >
+                  {{ person.organization_name }}
+                </td>
+                <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap hidden px-3 py-4 text-sm text-gray-500 sm:table-cell',
                     ]"
-                  >
-                    {{ person.town_city }}
-                  </td>
-                  <td
+                >
+                  {{ person.town_city }}
+                </td>
+                <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap hidden px-3 py-4 text-sm text-gray-500 lg:table-cell',
                     ]"
-                  >
-                    {{ person.type_rating }}
-                  </td>
-                  <td
+                >
+                  {{ person.type_rating }}
+                </td>
+                <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap px-3 py-4 text-sm text-gray-500',
                     ]"
-                  >
-                    {{ person.route }}
-                  </td>
-                  <td
+                >
+                  {{ person.route }}
+                </td>
+                <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8',
                     ]"
+                >
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                  >Edit<span class="sr-only">, {{ person.name }}</span></a
                   >
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                      >Edit<span class="sr-only">, {{ person.name }}</span></a
-                    >
-                  </td>
-                </tr>
+                </td>
+              </tr>
               </tbody>
             </table>
           </div>
         </div>
       </div>
 
-      <Debug :data="{ data }" />
+      <Debug :data="{ data }"/>
     </div>
   </div>
 </template>
---
+++ stores/data.ts
@@ -0,0 +1,109 @@
+export const routes_data = [
+  {
+    route: "Global Business Mobility: Senior or Specialist Worker",
+    key: "global_business_mobility_senior_or_specialist_worker",
+  },
+  {
+    route: "International Agreement",
+    key: "international_agreement",
+  },
+  {
+    route: "International Sportsperson",
+    key: "international_sportsperson",
+  },
+  {
+    route: "Charity Worker",
+    key: "charity_worker",
+  },
+  {
+    route: "Intra Company Transfers (ICT)",
+    key: "intra_company_transfers_ict",
+  },
+  {
+    route: "Creative Worker",
+    key: "creative_worker",
+  },
+  {
+    route: "Intra-company Routes",
+    key: "intra_company_routes",
+  },
+  {
+    route: "Global Business Mobility: Service Supplier",
+    key: "global_business_mobility_service_supplier",
+  },
+  {
+    route: "Government Authorised Exchange",
+    key: "government_authorised_exchange",
+  },
+  {
+    route: "Seasonal Worker",
+    key: "seasonal_worker",
+  },
+  {
+    route: "Religious Worker",
+    key: "religious_worker",
+  },
+  {
+    route: "Scale-up",
+    key: "scale_up",
+  },
+  {
+    route: "Global Business Mobility: Graduate Trainee",
+    key: "global_business_mobility_graduate_trainee",
+  },
+  {
+    route: "Global Business Mobility: UK Expansion Worker",
+    key: "global_business_mobility_uk_expansion_worker",
+  },
+  {
+    route: "Tier 2 Ministers of Religion",
+    key: "tier_2_ministers_of_religion",
+  },
+  {
+    route: "Global Business Mobility: Secondment Worker",
+    key: "global_business_mobility_secondment_worker",
+  },
+  {
+    route: "Skilled Worker",
+    key: "skilled_worker",
+  },
+];
+
+export const type_rating = [
+  {
+    type_rating: "Worker (A (SME+))",
+    key: "worker_a_sme",
+  },
+  {
+    type_rating: "Worker (UK Expansion Worker: Provisional)",
+    key: "worker_uk_expansion_worker_provisional",
+  },
+  {
+    type_rating: "Temporary Worker (A (Premium))",
+    key: "temporary_worker_a_premium",
+  },
+  {
+    type_rating: "Temporary Worker (B rating)",
+    key: "temporary_worker_b_rating",
+  },
+  {
+    type_rating: "Worker (A (Premium))",
+    key: "worker_a_premium",
+  },
+  {
+    type_rating: "Worker (B rating)",
+    key: "worker_b_rating",
+  },
+  {
+    type_rating: "Temporary Worker (A (SME+))",
+    key: "temporary_worker_a_sme",
+  },
+  {
+    type_rating: "Worker (A rating)",
+    key: "worker_a_rating",
+  },
+  {
+    type_rating: "Temporary Worker (A rating)",
+    key: "temporary_worker_a_rating",
+  },
+];
