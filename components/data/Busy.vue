9c1f2d9d3aaa49099df4f7a2f3786019cc061331
barehands-io <>
2023-10-07 00:32:58 +0100

    table works advanced table

--- components/data/Busy.vue
+++ components/data/Busy.vue
@@ -1,23 +1,18 @@
 <template>
   <div>
     <div class="flex justify-center h-screen items-center">
-    <div class="loading">
-      <div class="bar"></div>
-      <div class="bar"></div>
-      <div class="bar"></div>
-    </div>
+      <div class="loading">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
-
-
-
 .loading {
   display: flex;
   gap: 5px;
@@ -30,11 +25,11 @@
   animation-duration: 0.6s;
   animation-timing-function: ease-in-out;
   animation-iteration-count: infinite;
-  @apply bg-primary-500  rounded-2xl
+  @apply bg-green-500  rounded-2xl;
 }
 
 .bar:nth-child(1) {
-  animation-delay: 0.0s;
+  animation-delay: 0s;
 }
 
 .bar:nth-child(2) {
@@ -46,7 +41,11 @@
 }
 
 @keyframes jump {
-  0%, 20%, 50%, 80%, 100% {
+  0%,
+  20%,
+  50%,
+  80%,
+  100% {
     transform: translateY(0);
   }
   40% {
@@ -56,6 +55,4 @@
     transform: translateY(-10px);
   }
 }
-
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import axios from "axios";
+import { onMounted, ref, watch } from "vue";
 import { useStore } from "~/stores";
 import { routes_data, type_rating } from "~/stores/data";
 
@@ -10,7 +11,6 @@ definePageMeta({
 
 const store = useStore();
 
-
 const form = ref<any>({
   organization_name: "",
   town_city: "",
@@ -19,178 +19,215 @@ const form = ref<any>({
   search: "",
 });
 
-
 const data = ref<any>();
 
 const isLoading = ref(false);
 
 const error = ref("");
 
+let timeout: number;
 
 function getData() {
   isLoading.value = true;
   error.value = "";
-  axios.get("http://localhost:3800/all")
-      .then((res) => {
-
-        data.value = res.data;
-        isLoading.value = false;
-      })
-      .catch((err) => {
-        error.value = err;
-        isLoading.value = false;
-      });
+  axios
+    .get("http://localhost:3800/all", {
+      params: {
+        organization_name: form.value.organization_name,
+        town_city: form.value.town_city,
+        type_rating: form.value.type_rating,
+        route: form.value.route,
+        search: form.value.search,
+      },
+    })
+    .then((res) => {
+      data.value = res.data;
+      isLoading.value = false;
+    })
+    .catch((err) => {
+      error.value = err;
+      isLoading.value = false;
+    });
 }
 
 onMounted(() => {
   getData();
 });
 
-
+watch(
+  () => form.value,
+  (value) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      getData();
+      console.log(value);
+    }, 500); // 500ms delay
+  },
+  {
+    deep: true,
+  },
+);
 </script>
 
 <template>
-  <div v-if="isLoading">
-    loading
-  </div>
+  {{ form }}
+  <DataBusy v-if="isLoading" />
 
-  <div
-      v-else-if="data?.data?.data"
-      class="container mx-auto">
+  <div v-else-if="data?.data?.data" class="container mx-auto">
     <div class="px-4 sm:px-6 lg:px-8">
       {{ data.data.meta }}
 
-  
       <div>
         <div class="grid grid-cols-2 gap-8">
-
           <div class="flex flex-col">
             <label>Route:</label>
 
-            <select class="border-2 rounded-md py-2">
-              <option v-for="(item, index) in routes_data" :key="index" :value="item.key"> {{ item.route }}</option>
+            <select v-model="form.route" class="border-2 rounded-md py-2">
+              <option
+                v-for="(item, index) in routes_data"
+                :key="index"
+                :value="item.route"
+              >
+                {{ item.route }}
+              </option>
             </select>
           </div>
           <div class="flex flex-col">
             <label>Type rating:</label>
 
-            <select class="border-2 rounded-md py-2">
-              <option v-for="(item, index) in type_rating" :key="index" :value="item.key"> {{
-                  item.type_rating
-                }}
+            <select v-model="form.type_rating" class="border-2 rounded-md py-2">
+              <option
+                v-for="(item, index) in type_rating"
+                :key="index"
+                :value="item.type_rating"
+              >
+                {{ item.type_rating }}
               </option>
             </select>
           </div>
-          <div class="flex flex-col">
+          <div class="flex flex-col form">
             <label> type_rating</label>
-            <input type="search" class="bg-gray-200 p-2 w-full"/>
-            <p>Search by Company Name town_city type_rating <p class="bg-gray-700">Route</p></p>
+            <input
+              v-model="form.search"
+              type="search"
+              class="bg-gray-200 p-2 w-full"
+            />
+            <p>Search by Company Name</p>
           </div>
         </div>
       </div>
+
+      <h1>
+        <span class="text-xl">Total Matches</span>
+        <span class="text-2xl pl-2">({{ data.data.meta.total }})</span>
+      </h1>
       <div class="mt-8 flow-root">
         <div class="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
           <div class="inline-block min-w-full py-2 align-middle">
             <table class="min-w-full border-separate border-spacing-0">
               <thead>
-              <tr>
-                <th
+                <tr>
+                  <th
                     scope="col"
                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
-                >
-                  Company Name
-                </th>
-                <th
+                  >
+                    Company Name
+                  </th>
+                  <th
                     scope="col"
                     class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
-                >
-                  town_city
-                </th>
-                <th
+                  >
+                    town_city
+                  </th>
+                  <th
                     scope="col"
                     class="sticky top-0 z-10 hidden border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
-                >
-                  type_rating
-                </th>
-                <th
+                  >
+                    type_rating
+                  </th>
+                  <th
                     scope="col"
                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                >
-                  Route
-                </th>
-                <th
+                  >
+                    Route
+                  </th>
+                  <th
                     scope="col"
                     class="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-3 pr-4 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
-                >
-                  <span class="sr-only">Edit</span>
-                </th>
-              </tr>
+                  >
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
               </thead>
               <tbody>
-              <tr
+                <tr
                   v-for="(person, personIdx) in data.data.data"
                   :key="person.email"
-              >
-                <td
+                >
+                  <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8',
                     ]"
-                >
-                  {{ person.organization_name }}
-                </td>
-                <td
+                  >
+                    {{ person.organization_name }}
+                  </td>
+                  <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap hidden px-3 py-4 text-sm text-gray-500 sm:table-cell',
                     ]"
-                >
-                  {{ person.town_city }}
-                </td>
-                <td
+                  >
+                    {{ person.town_city }}
+                  </td>
+                  <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap hidden px-3 py-4 text-sm text-gray-500 lg:table-cell',
                     ]"
-                >
-                  {{ person.type_rating }}
-                </td>
-                <td
+                  >
+                    {{ person.type_rating }}
+                  </td>
+                  <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'whitespace-nowrap px-3 py-4 text-sm text-gray-500',
                     ]"
-                >
-                  {{ person.route }}
-                </td>
-                <td
+                  >
+                    {{ person.route }}
+                  </td>
+                  <td
                     :class="[
                       personIdx !== data.data.data.length - 1
                         ? 'border-b border-gray-200'
                         : '',
                       'relative whitespace-nowrap py-4 pr-4 pl-3 text-right text-sm font-medium sm:pr-8 lg:pr-8',
                     ]"
-                >
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                  >Edit<span class="sr-only">, {{ person.name }}</span></a
                   >
-                </td>
-              </tr>
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                      >Edit<span class="sr-only">, {{ person.name }}</span></a
+                    >
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
         </div>
       </div>
 
-      <Debug :data="{ data }"/>
+      <DataPagination :data="data.data.meta" />
+
+      <div class="pb-20"></div>
+
+      <Debug :data="{ data }" />
     </div>
   </div>
 </template>
